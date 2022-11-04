44fa2ba75423144fbe95fc2aba5c0b53946a2914
mikzanco <mikyzancolo97@gmail.com>
2022-11-04 17:25:24 +0100

    add is find per cercare se la task esiste già e aggiunti commenti in js

--- js/script.js
+++ js/script.js
@@ -27,9 +27,11 @@ createApp({
         removeTask(index){
             this.errorString= '';
             if(this.tasks[index].done){
+                // se la task risulta svolta (done) va a cancellare la task
                 this.tasks.splice(index, 1);
             }else{
                 console.log('error');
+                // se la task non è ancora stata svolta mi viene riportato un messaggio di errore
                 this.errorString = 'Attento devi fare il task prima di eliminarlo'
             }
         },
@@ -38,21 +40,30 @@ createApp({
             if(this.newMessageString.length < 5){
                 this.errorString = 'Attenzione la task aggiunta ha meno di 5 caratteri e non va bene';
                 
-            }else if(this.newMessageString === tasks.text){
                 
-                console.log('error');
-            }else{
-                const newTask ={
-                    text: this.newMessageString,
-                    done: false,
-                }
-                this.tasks.unshift(newTask);
-                this.newMessageString = '';
 
-                // verificare se la nuova task che si va ad inserire è gia presente nelel task 
-                
+            }else{
+
+                const isFind = this.tasks.find(item => item.text === this.newMessageString);
+                this.errorString ='';
+                if (isFind) {
+                    // verificare se la nuova task che si va ad inserire è gia presente nelle task e quindi da errore
+                    this.errorString = 'Attenzione hai già inseirto questa task!';
+                }else{
+                    // se non si presentano gli errori di cui sopra va a creare una nuova task con text il nuovo messaggio e con done false
+                    const newTask ={
+                        text: this.newMessageString,
+                        done: false,
+                    }
+                    // andiamo a posizionare la task nuova in cima alle altre task
+                    this.tasks.unshift(newTask);
+                    // ripuliamo l'input
+                    this.newMessageString = '';
+
+                    
+                    
+                }
             }
         }
-        
     }
 }).mount('#app')
