a33361dfdb6a669016483878edfe74322a59a267
mikzanco <mikyzancolo97@gmail.com>
2022-11-04 15:43:29 +0100

    add data in vuejs e stampati in pagina attraverso l'html con v-for

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ const{createApp} = Vue;
 createApp({
     data(){
         return{
-            logoImg: 'img/logo.png'
+            logoImg: 'img/logo.png',
+            tasks:[
+                {
+                    text: 'Portare fuori la spazzatura',
+                    done: false
+                },
+                {
+                    text: 'Andare a correre',
+                    done: true
+                },
+                {
+                    text: 'Studiare Bootsrap',
+                    done: false
+                },
+            ]
         }
+    },
+    methods:{
+        
     }
 }).mount('#app')
